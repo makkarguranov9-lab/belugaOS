e1a7d21922d608a812a5aff98868073639bf69b6
beluga <makkarguranov9@gmail.com>
2026-02-21 12:15:45 +0300

    Add multilingual support to cmd.bat
    Updated cmd.bat to support multilingual options and improved language selection prompts.

--- cmd.bat
+++ cmd.bat
@@ -1,23 +1,32 @@
 @echo off
+:: Включаем поддержку UTF-8 для корректного отображения русского языка
+chcp 65001 >nul
 setlocal enabledelayedexpansion
-title Beluga OS v1.3.1 - Windows Edition
-color 0B
+title Beluga OS v1.3.1 - Multilingual Edition
 
-:: --- Данные ---
+:lang_select
+cls
+echo Select Language / Выберите язык:
+echo 1. English
+echo 2. Русский
+set /p "ln=>> "
+if "%ln%"=="1" (set "lang=en") else (if "%ln%"=="2" (set "lang=ru") else (goto lang_select))
+
+:: --- Данные системы ---
 set "USER_ID=beluga"
 set "PASS_KEY=admin"
-set "VERSION=1.3.1-STABLE"
 set "KING=Beluga Edits"
 set "ADMIN=pirozhok_75"
 
 :login
 cls
-echo Access Key for %USER_ID%:
+if "%lang%"=="ru" (echo Введите ключ доступа для %USER_ID%:) else (echo Access Key for %USER_ID%:)
 set /p "input_pass="
 if not "!input_pass!"=="%PASS_KEY%" (
     color 0C
-    echo ERR: ACCESS DENIED
+    if "%lang%"=="ru" (echo ОШИБКА: ДОСТУП ЗАПРЕЩЕН) else (echo ERR: ACCESS DENIED)
     pause
+    color 0B
     goto login
 )
 
@@ -27,14 +36,18 @@ color 0B
 echo   ____  ______ _      _    _  _____  
 echo  ^|  _ \^|  ____^| ^|    ^| ^|  ^| ^|/ ____^| 
 echo  ^| ^|_) ^| ^|__  ^| ^|    ^| ^|  ^| ^| ^|  __  
-echo  ^|  _ ^<^|  __^| ^| ^|    ^| ^|  ^| ^| ^| ^|_ ^| / /\ \ 
-echo  ^| ^|_) ^| ^|____^| ^|____^| ^|__^| ^| ^|__^| ^|/ ____ \ 
-echo  ^|____/^|______^|______^|\____/ \_____/_/    \_\ OS
+echo  ^|  _ ^<^|  __^| ^| ^|    ^| ^|  ^| ^| ^| ^|_ ^| / /\\ \\ 
+echo  ^| ^|_) ^| ^|____^| ^|____^| ^|__^| ^| ^|__^| ^|/ ____ \\ 
+echo  ^|____/^|______^|______^|\____/ \\_____/_/    \\_\\ OS
 echo.
-echo  ^> v.%VERSION% ^| OP: %USER_ID% ^| NET: ONLINE
-echo  ^> КОРОЛЬ: %KING% ^| АДМИН: %ADMIN%
+if "%lang%"=="ru" (
+    echo  ^> v.1.3.1-STABLE ^| ОПЕРАТОР: %USER_ID% ^| СЕТЬ: ONLINE
+    echo  ^> КОРОЛЬ: %KING% ^| АДМИН: %ADMIN%
+) else (
+    echo  ^> v.1.3.1-STABLE ^| OP: %USER_ID% ^| NET: ONLINE
+    echo  ^> KING: %KING% ^| ADMIN: %ADMIN%
+)
 echo  --------------------------------------
-echo.
 
 :shell
 set "cmd="
@@ -42,51 +55,39 @@ set /p "cmd=%USER_ID%@beluga:~$ "
 
 if "!cmd!"=="help" (
     echo.
-    echo МОДУЛИ:
-    echo  weather - Погода (нужен интернет)
-    echo  web     - Открыть браузер
-    echo  info    - О создателях
-    echo  hack    - Режим взлома
-    echo  clear   - Очистить экран
-    echo  exit    - Выход
-    echo.
+    if "%lang%"=="ru" (
+        echo МОДУЛИ:
+        echo  weather - Погода
+        echo  web     - Браузер
+        echo  lang    - Сменить язык
+        echo  clear   - Очистить
+        echo  exit    - Выход
+    ) else (
+        echo MODULES:
+        echo  weather - Weather
+        echo  web     - Browser
+        echo  lang    - Change Language
+        echo  clear   - Clear screen
+        echo  exit    - Exit
+    )
     goto shell
 )
 
+if "!cmd!"=="lang" goto lang_select
+
 if "!cmd!"=="weather" (
-    echo [*] Получение данных о погоде...
     curl wttr.in?0
-    echo.
     goto shell
 )
 
 if "!cmd!"=="web" (
-    set /p "url=Введите URL (например, google.com): "
+    if "%lang%"=="ru" (set /p "url=Введите URL: ") else (set /p "url=Enter URL: ")
     start https://!url!
-    goto shell
-)
-
-if "!cmd!"=="info" (
-    echo.
-    echo --- BELUGA OS TEAM ---
-    echo Создатель (Король): %KING%
-    echo Администратор:      %ADMIN%
-    echo Версия системы:     %VERSION%
-    echo.
-    goto shell
-)
-
-if "!cmd!"=="hack" (
-    echo Bruteforcing...
-    timeout /t 1 >nul
-    echo Success! Access granted by %ADMIN%.
     goto shell
 )
 
 if "!cmd!"=="clear" goto main_menu
 if "!cmd!"=="exit" exit
 
-:: Если команда не из списка, пробуем выполнить как системную
 !cmd!
-echo.
 goto shell
